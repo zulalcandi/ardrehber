f0db0cf2bf8b26f163499c9305eaaa840c3ef01d
zulalcandi <zulalcandi@gmail.com>
2022-05-06 09:38:36 +0300

    github deneme

--- ArdRehber/ArdRehber/Entities/User.cs
+++ ArdRehber/ArdRehber/Entities/User.cs
@@ -9,9 +9,7 @@
             public string Email { get; set; }
             public string Password { get; set; }
             public string RefreshToken { get; set; }=String.Empty;
-
             public DateTime? RefreshTokenEndDate { get; set; }
-
             public string UserType { get; set; }
 
     }
