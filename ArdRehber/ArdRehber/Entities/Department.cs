65ecd87ab5657713fa161c0a30194b9d90d1cc98
zulalcandi <zulalcandi@gmail.com>
2022-05-09 18:03:58 +0300

    icollection silindi

--- ArdRehber/ArdRehber/Entities/Department.cs
+++ ArdRehber/ArdRehber/Entities/Department.cs
@@ -10,7 +10,7 @@ namespace ArdRehber.Entities
         [StringLength(50)]
         public string DepartmentName { get; set; }
 
-         public virtual ICollection<Person> Persons { get; set; }
+       //  public virtual ICollection<Person> Persons { get; set; }
 
 
 
