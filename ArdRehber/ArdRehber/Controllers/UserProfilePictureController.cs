65881dbcd5b90b00af1d7c40614081b3b6360268
zulalcandi <zulalcandi@gmail.com>
2022-05-26 13:23:28 +0300

    profile picture update yapıldı

--- ArdRehber/ArdRehber/Controllers/UserProfilePictureController.cs
+++ ArdRehber/ArdRehber/Controllers/UserProfilePictureController.cs
@@ -13,31 +13,39 @@ namespace ArdRehber.Controllers
     [Authorize]
     public class UserProfilePictureController : BaseController
     {
-        //public Image LoadImage()
-        //{
-        //    //data:image/gif;base64,
-        //    //this image is a single pixel (black)
-        //   // byte[] bytes = Convert.FromBase64String("iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAANCSURBVEiJtZZPbBtFFMZ/M7ubXdtdb1xSFyeilBapySVU8h8OoFaooFSqiihIVIpQBKci6KEg9Q6H9kovIHoCIVQJJCKE1ENFjnAgcaSGC6rEnxBwA04Tx43t2FnvDAfjkNibxgHxnWb2e/u992bee7tCa00YFsffekFY+nUzFtjW0LrvjRXrCDIAaPLlW0nHL0SsZtVoaF98mLrx3pdhOqLtYPHChahZcYYO7KvPFxvRl5XPp1sN3adWiD1ZAqD6XYK1b/dvE5IWryTt2udLFedwc1+9kLp+vbbpoDh+6TklxBeAi9TL0taeWpdmZzQDry0AcO+jQ12RyohqqoYoo8RDwJrU+qXkjWtfi8Xxt58BdQuwQs9qC/afLwCw8tnQbqYAPsgxE1S6F3EAIXux2oQFKm0ihMsOF71dHYx+f3NND68ghCu1YIoePPQN1pGRABkJ6Bus96CutRZMydTl+TvuiRW1m3n0eDl0vRPcEysqdXn+jsQPsrHMquGeXEaY4Yk4wxWcY5V/9scqOMOVUFthatyTy8QyqwZ+kDURKoMWxNKr2EeqVKcTNOajqKoBgOE28U4tdQl5p5bwCw7BWquaZSzAPlwjlithJtp3pTImSqQRrb2Z8PHGigD4RZuNX6JYj6wj7O4TFLbCO/Mn/m8R+h6rYSUb3ekokRY6f/YukArN979jcW+V/S8g0eT/N3VN3kTqWbQ428m9/8k0P/1aIhF36PccEl6EhOcAUCrXKZXXWS3XKd2vc/TRBG9O5ELC17MmWubD2nKhUKZa26Ba2+D3P+4/MNCFwg59oWVeYhkzgN/JDR8deKBoD7Y+ljEjGZ0sosXVTvbc6RHirr2reNy1OXd6pJsQ+gqjk8VWFYmHrwBzW/n+uMPFiRwHB2I7ih8ciHFxIkd/3Omk5tCDV1t+2nNu5sxxpDFNx+huNhVT3/zMDz8usXC3ddaHBj1GHj/As08fwTS7Kt1HBTmyN29vdwAw+/wbwLVOJ3uAD1wi/dUH7Qei66PfyuRj4Ik9is+hglfbkbfR3cnZm7chlUWLdwmprtCohX4HUtlOcQjLYCu+fzGJH2QRKvP3UNz8bWk1qMxjGTOMThZ3kvgLI5AzFfo379UAAAAASUVORK5CYII=");
-
-        //   // Image image;
-
-        //   // return image;
-        //}
-
-
         private readonly DataContext _context;
 
         public UserProfilePictureController(DataContext context)
         {
             _context = context;
         }
-        //public static string Base64Decode(string base64EncodedData)
-        //{
-        //    byte[] base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
-        //    return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+
+        private async Task<Image> AddImage(ImageDto model)
+        {
+            Image img = new Image();
+            img.UserId = GetUserId();
+            img.ImageBase64 = Convert.FromBase64String(model.ImageBase64);
+
+            _context.Images.Add(img);
+            await _context.SaveChangesAsync();
+            return img;
+
+        }
+
+        private async Task<Image> UpdateImage(ImageDto model)
+        {
+            var entity = _context.Images.FirstOrDefault(e => e.Id == model.Id);
+
+            entity.UserId = GetUserId();
+            entity.ImageBase64 = Convert.FromBase64String(model.ImageBase64); 
+           
+
+            _context.Images.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
 
 
-        //}
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Image>>> GetPersons()
@@ -65,27 +73,56 @@ namespace ArdRehber.Controllers
         {
             //byte[] bytes = Convert.FromBase64String("iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAANCSURBVEiJtZZPbBtFFMZ/M7ubXdtdb1xSFyeilBapySVU8h8OoFaooFSqiihIVIpQBKci6KEg9Q6H9kovIHoCIVQJJCKE1ENFjnAgcaSGC6rEnxBwA04Tx43t2FnvDAfjkNibxgHxnWb2e/u992bee7tCa00YFsffekFY+nUzFtjW0LrvjRXrCDIAaPLlW0nHL0SsZtVoaF98mLrx3pdhOqLtYPHChahZcYYO7KvPFxvRl5XPp1sN3adWiD1ZAqD6XYK1b/dvE5IWryTt2udLFedwc1+9kLp+vbbpoDh+6TklxBeAi9TL0taeWpdmZzQDry0AcO+jQ12RyohqqoYoo8RDwJrU+qXkjWtfi8Xxt58BdQuwQs9qC/afLwCw8tnQbqYAPsgxE1S6F3EAIXux2oQFKm0ihMsOF71dHYx+f3NND68ghCu1YIoePPQN1pGRABkJ6Bus96CutRZMydTl+TvuiRW1m3n0eDl0vRPcEysqdXn+jsQPsrHMquGeXEaY4Yk4wxWcY5V/9scqOMOVUFthatyTy8QyqwZ+kDURKoMWxNKr2EeqVKcTNOajqKoBgOE28U4tdQl5p5bwCw7BWquaZSzAPlwjlithJtp3pTImSqQRrb2Z8PHGigD4RZuNX6JYj6wj7O4TFLbCO/Mn/m8R+h6rYSUb3ekokRY6f/YukArN979jcW+V/S8g0eT/N3VN3kTqWbQ428m9/8k0P/1aIhF36PccEl6EhOcAUCrXKZXXWS3XKd2vc/TRBG9O5ELC17MmWubD2nKhUKZa26Ba2+D3P+4/MNCFwg59oWVeYhkzgN/JDR8deKBoD7Y+ljEjGZ0sosXVTvbc6RHirr2reNy1OXd6pJsQ+gqjk8VWFYmHrwBzW/n+uMPFiRwHB2I7ih8ciHFxIkd/3Omk5tCDV1t+2nNu5sxxpDFNx+huNhVT3/zMDz8usXC3ddaHBj1GHj/As08fwTS7Kt1HBTmyN29vdwAw+/wbwLVOJ3uAD1wi/dUH7Qei66PfyuRj4Ik9is+hglfbkbfR3cnZm7chlUWLdwmprtCohX4HUtlOcQjLYCu+fzGJH2QRKvP3UNz8bWk1qMxjGTOMThZ3kvgLI5AzFfo379UAAAAASUVORK5CYII=");
 
-            Image img = new Image();
-            img.UserId = GetUserId();
-            img.ImageBase64 = Convert.FromBase64String(model.ImageBase64);
+            //Image img = new Image();
+            //img.UserId = GetUserId();
+            //img.ImageBase64 = Convert.FromBase64String(model.ImageBase64);
 
-            _context.Images.Add(img);
-            await _context.SaveChangesAsync();
-            return Ok();
+            //_context.Images.Add(img);
+            //await _context.SaveChangesAsync();
+            //return Ok();
+
+            if (model.Id > 0)
+            {
+                var img = this.UpdateImage(model).Result;
+                //  personDto.Id = person.Id;
+
+            }
+            else
+            {
+                var img = this.AddImage(model).Result;
+           
+                model.Id = img.Id;
+            }
 
 
+            // else => ekleme
 
-            //    byte[] base64EncodedBytes = Convert.FromBase64String(imageDto.ImageBase64);
-            //    byte[] image;
-            //    _context.Users.Add(image);
-            //    await _context.SaveChangesAsync();
-            //    return image;
-            //}
+            return Ok(model);
 
 
+        }
 
+        // DELETE: api/Persons/5
 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteImage(int id)
+        {
+                var img = await _context.Images.FindAsync(id);
+                if (img == null)
+                {
+                    return NotFound();
+                }
 
+                _context.Images.Remove(img);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            
+        }
+
+        private bool ImageExists(int id)
+        {
+            return _context.Images.Any(e => e.Id == id);
         }
     }
 }
